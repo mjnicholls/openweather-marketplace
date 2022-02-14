b2ec1435b871e230e1c771a866e8a48a8ea3c2d9
M.J. Nicholls <marknicholls@openweather.co.uk>
2022-02-14 10:51:53 +0000

    first version 2

--- src/pages/NewHistoryBulk.js
+++ src/pages/NewHistoryBulk.js
@@ -4,7 +4,6 @@ import Map from "../components/Map";
 import placeMarker from "../utils/placeMarker";
 import Location from "../components/Location";
 import { useSelector } from "react-redux";
-import moment from "moment-timezone";
 
 const selectCurrency = (state) => state.auth.currency;
 const selectInvoice = (state) => state.auth.invoiceInfo;
@@ -14,8 +13,6 @@ const NewHistoryBulk = () => {
   const mapRef = useRef(null);
   const searchBoxRef = useRef();
   const invoice = useSelector(selectInvoice);
-
-  moment.tz.setDefault("America/Los_Angeles");
 
   const [error, setError] = useState({});
 
